bc51d1f23fef21bd5444388c40976c93d256c490
Danny Haque <dannyy.haque@gmail.com>
2019-01-08 17:30:10 +0100

    Added comments to extra scripts

--- createfiles.sh
+++ createfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#This script is simply to create new files, add the appropirate permissions and add the shebang in the beginning of the file.
 cd $HOME/bash-scripts
 echo -n "What do you want to name the file? "
 read file
