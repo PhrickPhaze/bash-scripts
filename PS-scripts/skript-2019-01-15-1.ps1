d8ca620401680f745b7afe38d4c80553ae3ec9f3
PhrickPhaze <dannyy.haque@gmail.com>
2019-01-15 11:27:16 +0100

    Added comments to script

--- PS-scripts/skript-2019-01-15-1.ps1
+++ PS-scripts/skript-2019-01-15-1.ps1
@@ -1,9 +1,14 @@
-﻿$cert = New-SelfSignedCertificate -Subject "Dannys certifikat” -Type CodeSigningCert -CertStoreLocation Cert:\LocalMachine\My
-Export-Certificate -Cert $cert -FilePath C:\temp\rootcert.crt
-Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath C:\temp\rootcert.crt
-# cd Cert:\CurrentUser\Root
-# ls
-'This is a certificate' | Out-File -FilePath C:\temp\rootcert.ps1
-Set-AuthenticodeSignature -FilePath C:\temp\rootcert.ps1 -Certificate $cert
+﻿# First the variable is set to create and sign a new certificate located in Cert:\LocalMachine\My
+$cert = New-SelfSignedCertificate -Subject "Dannys certifikat” -Type CodeSigningCert -CertStoreLocation Cert:\LocalMachine\My
 
-$cert
+# Then it needs to be exported to a file on a file system
+Export-Certificate -Cert $cert -FilePath C:\temp\rootcert.crt
+
+# Here the certificate file is imported to the root certificate directory from the file path
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath C:\temp\rootcert.crt
+
+# Here the script adds content to another script because scripts with 4 bytes or less cannot be certified
+'This is a certificate' | Out-File -FilePath C:\temp\rootcert.ps1
+
+# Finally the script certifies a script with the newly create certificate
+Set-AuthenticodeSignature -FilePath C:\temp\rootcert.ps1 -Certificate $cert
