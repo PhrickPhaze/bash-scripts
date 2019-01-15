1510fc45fd6670b58ee760c345a704188cc8f586
PhrickPhaze <dannyy.haque@gmail.com>
2019-01-15 11:18:21 +0100

    Uploaded certifiate script

---
+++ PS-scripts/skript-2019-01-15-1.ps1
@@ -0,0 +1,9 @@
+﻿$cert = New-SelfSignedCertificate -Subject "Dannys certifikat” -Type CodeSigningCert -CertStoreLocation Cert:\LocalMachine\My
+Export-Certificate -Cert $cert -FilePath C:\temp\rootcert.crt
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath C:\temp\rootcert.crt
+# cd Cert:\CurrentUser\Root
+# ls
+'This is a certificate' | Out-File -FilePath C:\temp\rootcert.ps1
+Set-AuthenticodeSignature -FilePath C:\temp\rootcert.ps1 -Certificate $cert
+
+$cert
