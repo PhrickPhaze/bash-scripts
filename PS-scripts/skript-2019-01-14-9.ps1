18c405402e3b811724e8888e1ee88ed656a75083
unknown <Administrator@VT17_1.local>
2019-01-16 14:26:42 +0100

    Added scripts to the repository

---
+++ PS-scripts/skript-2019-01-14-9.ps1
@@ -0,0 +1,16 @@
+﻿$a = "Total amount of successful login attempts"
+$a1 = Get-EventLog -Log Security -EntryType SuccessAudit | where {$_.EventID -eq 4624} | measure
+
+$b = "Total amount of logins via keyboard" 
+$b1 = Get-winevent -FilterHashtable @{logname='security'; id=4624} | where {$_.properties[8].value -eq 2} | measure
+
+$c = "Total amount of logins via the network" 
+$c1 = Get-winevent -FilterHashtable @{logname='security'; id=4624} | where {$_.properties[8].value -eq 3} | measure
+
+$d = "Total amount of unlocked logins" 
+$d1 = Get-winevent -FilterHashtable @{logname='security'; id=4624} | where {$_.properties[8].value -eq 7} | measure
+
+$e = "Total amount of failed login attempts" 
+$e1 = Get-EventLog -Log Security -EntryType FailureAudit | where {$_.EventID -eq 4624} | measure
+
+$a,$a1,$b,$b1,$c,$c1,$d,$d1,$e,$e1 | Out-File -FilePath C:\temp\Logins.txt
