4d9f37af548451b347e667038b73142116667188
Danny Haque <dannyy.haque@gmail.com>
2019-01-17 14:41:38 +0100

    Updated name standard and syntax

--- Lab1/olddestroyter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#This script deletes all *.txt.old files in the current location. It is a tool for testing the '20190107_Lab1h.sh' script.
-rm *.txt.old
-echo "All *.txt.old files have been deleted!"
