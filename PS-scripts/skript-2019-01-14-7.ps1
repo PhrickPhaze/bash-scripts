2afbfede5dda522dd7b082605d5c263121b1a4b8
unknown <Administrator@VT17_1.local>
2019-01-18 14:11:52 +0100

    Changed syntax and cleaned up clutter

--- PS-scripts/skript-2019-01-14-7.ps1
+++ PS-scripts/skript-2019-01-14-7.ps1
@@ -15,13 +15,13 @@ New-Item -Path C:\temp\fel.html
 
 # COMMENT: This command lists the last 100 entries in the system logs sorted in a uniquely manner
 # CODE:
-# Get-EventLog -LogName System -Newest 100 | Sort-Object -Unique
+# Get-EventLog -LogName System -Newest 100 -EntryType Error,Warning | Group-Object EntryType
 
 # COMMENT: This command is split into two parts. The first one is identical to the first command in the script.
 #          The second part is an if statement where it'll print out a warning message if the amount exceeds 10 entries.
 # CODE:
-# Get-EventLog -Logname system -EntryType Error,Warning | measure
-# If($test.Count -gt "10") {Write-Host "There are more than 10 warnings/errors!"}
+# G$body4 = Get-EventLog -Logname system -EntryType Error,Warning | measure
+# If($body4.Count -gt 10) {$outfile = "There are more than 10 warnings/errors!"}
 
 # HTML formatting
 $Head = @"
@@ -53,10 +53,12 @@ $Head = @"
 # Inserting the commands above in HTML and out it to the HTML file created in the begenning of the script
 [string]$body1 = Get-EventLog -Logname system -Entrytype Error,Warning | measure | ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>Amount of errors and warnings</h4></font>" | Out-File -FilePath C:\temp\fel.html
 [string]$body2 = Get-EventLog -LogName System -After (Get-date).AddHours(-24)| ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>Last 24 hours report</h4></font>" | Out-File -FilePath C:\temp\fel.html -Append
-[string]$body3 = Get-EventLog -LogName System -Newest 100 | Sort-Object -Unique | ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>Sort objects uniquely</h4></font>" | Out-File -FilePath C:\temp\fel.html -Append
-[string]$body3 = Get-EventLog -Logname system -EntryType Error,Warning | measure
-If($test.Count -gt "10") {Write-Host "There are more than 10 warnings/errors!"}
-ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>If statement</h4></font>" | Out-File -FilePath C:\temp\fel.html -Append
+[string]$body3 = Get-EventLog -LogName System -Newest 100 -EntryType Error,Warning | Group-Object EntryType | ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>Sort objects uniquely</h4></font>" | Out-File -FilePath C:\temp\fel.html -Append
+$body4 = Get-EventLog -Logname system -EntryType Error,Warning | measure
+If($body4.Count -gt 10) {$outfile = "There are more than 10 warnings/errors!"}
+ConvertTo-HTML -head $Head -Body "<font color=`"Black`"><h4>If statement <br><br>$outfile</h4></font>" | Out-File -FilePath C:\temp\fel.html -Append
+
+ 
 
 # Running the script up until now implements all commands (in the 'a' assignment) to single html file with every output.
 
